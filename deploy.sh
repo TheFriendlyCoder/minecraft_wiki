55cac3548b5d1ac42cbf31ede95cfb6d1addc1fe
Kevin Phillips <thefriendlycoder@gmail.com>
2022-02-20 23:49:13 -0400

    minor bug fix to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ USER=kevin
 HOST=192.168.2.25
 DIR=/var/www/html
 
-hugo && rsync -rlpgoDvz --delete --group=www-data public/ ${USER}@${HOST}:${DIR}
+# NOTE: we have to preserve the www-data group to ensure
+#       Apache will still be able to read the content
+hugo && rsync -avz --delete --group=www-data public/ ${USER}@${HOST}:${DIR}
